afa7c1282f8d70021b456da2e7a7147cd5e41755
aristakesyanl <60519263+aristakesyanl@users.noreply.github.com>
2022-05-22 19:23:34 +0400

    Update VirtualMachine.cpp

--- VirtualMachine.cpp
+++ VirtualMachine.cpp
@@ -266,18 +266,3 @@ void VirtualMachine::watchRegisters(){
 	std::cout<<"Output: "<<R[8]<<std::endl;
 
 }
-
-int main(int argc, char* argv[]) {
-    
-    if(argc<2){
-        std::cerr<<"No Arguments Provided"<<std::endl;
-    }
-    
-    for (int i = 1; i < argc; i++) {
-
-        std::string str(argv[i]);
-        VirtualMachine a(str);
-        a.run();
-        a.watchRegisters();
-    }
-}
